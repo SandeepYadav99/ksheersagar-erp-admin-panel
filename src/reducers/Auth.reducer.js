df65cb8d09850756177821d521dca150570f40ed
charanjeet <charanjeet@electrovese.com>
2024-04-02 11:07:46 +0530

    auth reducer change

--- src/reducers/Auth.reducer.js
+++ src/reducers/Auth.reducer.js
@@ -25,24 +25,24 @@ const initialState = {
 
 export default function (state = JSON.parse(JSON.stringify(initialState)), action) {
     switch (action.type) {
-        case AUTH_USER : {
+        case 'AUTH_USER' : {
             return {...state,
                 is_authenticated: true,
                 user: action.payload,
                 role: action?.payload?.role,
             }
         }
-        case LOGOUT_USER: {
+        case 'LOGOUT_USER': {
             return {...state, ...(JSON.parse(JSON.stringify(initialState)))};
         }
-        case GET_PROFILE_INIT: {
+        case 'GET_PROFILE_INIT': {
             const tempProfile = state.user_profile;
             tempProfile.is_fetching = true;
             return {
                 ...state, user_profile: tempProfile
             };
         }
-        case SET_PROFILE: {
+        case 'SET_PROFILE': {
             const tempProfile = action.payload;
             tempProfile.is_fetching = false;
             tempProfile.is_verified = true;
