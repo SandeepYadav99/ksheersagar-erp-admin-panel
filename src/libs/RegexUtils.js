78757a5571ba645b9f79cb91584920a35c2b4b5a
Sandeep Yadav <sandeepelectrovese@gmail.com>
2024-06-24 21:39:13 +0530

    REFACTOR -> Validate UPI ID

--- src/libs/RegexUtils.js
+++ src/libs/RegexUtils.js
@@ -1,98 +1,98 @@
 import LogUtils from "./LogUtils";
 
 const isUrl = (value) => {
-    return /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/.test(value);
-}
+  return /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/.test(
+    value
+  );
+};
 
 const isEmail = (value) => {
-    return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i.test(value);
-}
+  return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i.test(value);
+};
 
 const isAlphaNum = (value) => {
-    return /^[a-zA-Z0-9. ]*$/.test(value);
-}
+  return /^[a-zA-Z0-9. ]*$/.test(value);
+};
 
 const isAlphaNumChars = (value) => {
-    return /^[a-zA-Z0-9._&!\-@#+/:,;()%="' ]*$/.test(value);
-}
+  return /^[a-zA-Z0-9._&!\-@#+/:,;()%="' ]*$/.test(value);
+};
 
 const isAlpha = (value) => {
-    return /^[a-zA-Z ]*$/.test(value);
-}
+  return /^[a-zA-Z ]*$/.test(value);
+};
 
-const isUpiID =(value)=>{
-    return /^[a-zA-Z0-9._-]+@[a-zA-Z]{3,}\.[a-zA-Z]{2,}$/.test(value)
-}
+const isUpiID = (value) => {
+  return /^[a-zA-Z0-9.\-_]{2,256}@[a-zA-Z]{2,64}$/.test(value);
+};
 
 const isNum = (value) => {
-    return /^[0-9]*$/.test(value);
-}
+  return /^[0-9]*$/.test(value);
+};
 
 const isNumDec = (value) => {
-    return /^\d+(\.\d{1})?$/.test(value);
-}
+  return /^\d+(\.\d{1})?$/.test(value);
+};
 
 const isDate = (value) => {
-        return value instanceof Date && !isNaN(value);
-}
+  return value instanceof Date && !isNaN(value);
+};
 
 const isSpace = (value) => {
-    return /\s/.test(value);
-}
+  return /\s/.test(value);
+};
 
 const isAadhar = (value) => {
-    return /^[2-9]{1}[0-9]{3}[0-9]{4}[0-9]{4}$/.test(value);
-}
+  return /^[2-9]{1}[0-9]{3}[0-9]{4}[0-9]{4}$/.test(value);
+};
 
-const isAccountNum=(value)=>{
-    return /^\d{9,18}$/.test(value)
-}
+const isAccountNum = (value) => {
+  return /^\d{9,18}$/.test(value);
+};
 
-const IsIFSCCode=(value)=>{
-    return /^[A-Z]{4}0[A-Z0-9]{6}$/.test(value)
-}
+const IsIFSCCode = (value) => {
+  return /^[A-Z]{4}0[A-Z0-9]{6}$/.test(value);
+};
 // Account Number: ^\d{9,18}$
 // IFSC: regex = "^[A-Z]{4}0[A-Z0-9]{6}$";
-const IsVehicleNo=(value)=>{
-    return /^[A-Za-z]{2}.+\d{4}$/.test(value)
-}
+const IsVehicleNo = (value) => {
+  return /^[A-Za-z]{2}.+\d{4}$/.test(value);
+};
 const isAlphaNumeric = (text) => {
-  
-    return /^[a-z0-9 ]+$/i.test(text);
-  };
- 
+  return /^[a-z0-9 ]+$/i.test(text);
+};
+
 function validateUrl(value) {
-    return /^https?:\/\/[^\s/$.?#].[^\s]*$/i.test(value);
-  }
-  function validateUAN(uan) {
-    const regex = /^\d{12}$/;
-    return regex.test(uan);
-  }
+  return /^https?:\/\/[^\s/$.?#].[^\s]*$/i.test(value);
+}
+function validateUAN(uan) {
+  const regex = /^\d{12}$/;
+  return regex.test(uan);
+}
 
-  function validateESI(esiNumber) {
-    // const regex = /^[0-9]{2}-[0-9]{2}-[0-9]{6}-[0-9]{3}-[0-9]{4}$/;
-    const regex = /^\d{17}$/;
-    return regex.test(esiNumber);
-  }
+function validateESI(esiNumber) {
+  // const regex = /^[0-9]{2}-[0-9]{2}-[0-9]{6}-[0-9]{3}-[0-9]{4}$/;
+  const regex = /^\d{17}$/;
+  return regex.test(esiNumber);
+}
 
- 
 export {
-    isUrl,
-    isEmail,
-    isAlphaNum,
-    isNum,
-    isAlpha,
-    isDate,
-    isSpace,isAlphaNumChars,
-    isAadhar,
-    IsIFSCCode,
-    isAccountNum,
-    IsVehicleNo,
-    validateUrl,
-    isNumDec,
-    validateUAN,
-    validateESI,
-    isAlphaNumeric,
-    isUpiID
-   
+  isUrl,
+  isEmail,
+  isAlphaNum,
+  isNum,
+  isAlpha,
+  isDate,
+  isSpace,
+  isAlphaNumChars,
+  isAadhar,
+  IsIFSCCode,
+  isAccountNum,
+  IsVehicleNo,
+  validateUrl,
+  isNumDec,
+  validateUAN,
+  validateESI,
+  isAlphaNumeric,
+  isUpiID,
 };
