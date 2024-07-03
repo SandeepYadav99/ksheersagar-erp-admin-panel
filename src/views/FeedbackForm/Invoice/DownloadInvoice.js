b0dd75eb001b159aff924787d822ade71a9bcb8d
Sandeep Yadav <sandeepelectrovese@gmail.com>
2024-07-03 14:52:48 +0530

    REFACTOR -> INVOICE PAGE

--- src/views/FeedbackForm/Invoice/DownloadInvoice.js
+++ src/views/FeedbackForm/Invoice/DownloadInvoice.js
@@ -1,13 +1,12 @@
 import React, { useCallback, useState } from "react";
 import styles from "./styles.module.css";
 
-import { ButtonBase } from "@material-ui/core";
-import ic_star from "../../../assets/img/feedback/ic_star.png";
+
+
 import dummy_qr from "../../../assets/img/feedback/dummy qr.png";
-import ic_print from "../../../assets/img/feedback/ic_print.png";
-import ic_quantity from "../../../assets/img/feedback/ic_quantity.png";
+
 import ic_topnav_logo from "../../../assets/img/feedback/ic_topnav_logo.png";
-import ic_download from "../../../assets/img/feedback/ic_download.png";
+
 import ItemTable from "./ItemTable";
 const DownloadInvoice = () => {
   const [selectedRating, setSelectedRating] = useState(null);
@@ -27,7 +26,12 @@ const DownloadInvoice = () => {
       <div className={styles.headerContainer}>
         <div className={styles.logo}>
           <img src={ic_topnav_logo} alt="" width={166} height={34} />
+          <br />
+          <span className={styles.logoTitle}>Tax Invoice</span>
+          <br />
+          <span className={styles.logoInvoice}>(Digital Invoice)</span>
         </div>
+        <hr className={styles.hrLine} />
         <p className={styles.title}>Ksheer Sagar Sonarpura</p>
         <p className={styles.subTitle}>
           B 15/45, Sonarpura, Varanasi, Uttar Pradesh, 221001
@@ -44,89 +48,82 @@ const DownloadInvoice = () => {
         <p className={styles.subTitle}>
           FSSAI No: <strong>209456880011</strong>{" "}
         </p>
-        <p className={styles.subTitle}>
+        {/* <p className={styles.subTitle}>
           Website. <a href="https://ksheersagar.com/">https://ksheersagar.com/</a>{" "}
-        </p>
-        <div className={styles.gaps} />
-        <p className={styles.title}>Summary</p>
-        <p className={styles.subTitle}>
-          Invoice Number: <strong>INV - BC/2023/08/21</strong>{" "}
-        </p>
-        <p className={styles.subTitle}>
-        Date:<strong>2/08/2023 | 11:00 AM</strong>{" "}
-        </p>
-        <p className={styles.subTitle}>
-        Temp Sales Order:<strong>KS/BC/2023/08/200</strong>{" "}
-        </p>
-        <p className={styles.subTitle}>
-        Cashier:<strong>Abhinav Sharma</strong>{" "}
-        </p>
-      
-        <div className={styles.gaps} />
-        <p className={styles.title}>Customer Details</p>
-        <p className={styles.subTitle}>
-          <strong>Aakash Singh</strong>
-        </p>
-        <p className={styles.subTitle}>
-          <strong>9887698342</strong>
-        </p>
-        <p className={styles.subTitle}>
-          GSTIN:<strong> 33881091A35</strong>
-        </p>
-        <div className={styles.gaps} />
-        <hr />
-      
+        </p> */}
+        {/* Detail INvoice */}
+        <hr className={styles.hrLine} />
         <div className={styles.gaps} />
         <p className={styles.title}>Invoice Details</p>
-        <div className={styles.gaps} />
-        <div className={styles.footer}>
-          <p className={styles.subTitle}>
-            Total Amount:
-            <br /> <strong> ₹1000</strong>
-          </p>
-          <p>
-            <span className={styles.subTitle}>No. of Items: 02</span> <br />
-            <span className={styles.subTitle}>No. of Boxes: 20</span>
-          </p>
-        </div>
-{/* Item List Invoice */}
-<div >
+        <p className={styles.subTitle}>
+          Invoice No.:<strong>INV-BC/2023/08/21</strong>{" "}
+        </p>
+        <p className={styles.subTitle}>
+          Date & Time:<strong>28/06/2024 ǀ 01:44 PM</strong>{" "}
+        </p>
+        <p className={styles.subTitle}>
+          Cashier:<strong>Abhinav Sharma</strong>{" "}
+        </p>
+        <p className={styles.subTitle}>
+          Mode of Payment:<strong>Cash</strong>{" "}
+        </p>
+        <p className={styles.subTitle}>
+          Place of Supply:<strong>Varanasi, Uttar Pradesh</strong>{" "}
+        </p>
 
-<ItemTable/>
-</div>
-{/* Item List Invoice End*/}
-       
+        {/* End */}
         <div className={styles.gaps} />
-        <div className={styles.footer}>
-          <p className={styles.subTitle}>Item Total</p>
-          <p className={styles.subTitle}>
-            <strong>₹500</strong>{" "}
-          </p>
-        </div>
-        <div className={styles.footer}>
-          <p className={styles.subTitle}>IGST</p>
-          <p className={styles.subTitle}>
-            <strong>₹500</strong>{" "}
-          </p>
-        </div>
-        <hr />
-        <div className={styles.footer}>
-          <p className={styles.subTitle}>Total Amount Paid</p>
-          <p className={styles.subTitle}>
-            <strong>₹500</strong>{" "}
-          </p>
-        </div>
-        <p className={styles.subTitle}>Nine Hundred and Eighty Seven Rupees only</p>
+        <hr className={styles.hrLine} />
+        {/* End DETAILL */}
+        {/* customer Detail */}
+        <p className={styles.title}>Customer Details</p>
+        <p className={styles.subTitle}>
+          Name:<strong>Abhinav Sharma</strong>{" "}
+        </p>
+        <p className={styles.subTitle}>
+          Contact No:<strong> 9805798997</strong>{" "}
+        </p>
+        <p className={styles.subTitle}>
+          Company Name:<strong> XYZ Bakery</strong>{" "}
+        </p>
+        <p className={styles.subTitle}>
+          GSTIN:<strong> 23AAECH789D1Z8</strong>{" "}
+        </p>
+
         <div className={styles.gaps} />
-        <p className={styles.scanTitle}>Scan below to get the bill information</p>
+        <hr className={styles.hrLine} />
+        {/* End Customer Detail */}
+        <p className={styles.title}>Summary</p>
+        <p className={styles.subTitle}>
+          No. of Items: <strong>03</strong>{" "}
+        </p>
+        <p className={styles.subTitle}>
+          No. of Boxes: <strong>04</strong>{" "}
+        </p>
+        {/* Item List Invoice */}
+        <div>
+          <ItemTable />
+        </div>
+        {/* Item List Invoice End*/}
+
+        <p className={styles.thankyounote}>Thank you for Shopping with us!</p>
+        <p className={styles.subTitlePara}>
+        For any queries please call/email us on customer care
+        M: 7311122332, E: customer.care@ksheersagar.com
+        Visit us at <a href="www.ksheersagar.com" target="_blank"rel="noopener">www.ksheersagar.com</a>
+        </p>
+        <hr className={styles.hrLine}/>
         <div className={styles.download}>
-          <p className={styles.subTitle}>
+          <p className={styles.subTitleQr}>
             Now get a digital copy of your invoice
           </p>
           <div className={styles.footerlinkqr}>
-            
             <img src={dummy_qr} alt="" width={110} height={110} />
-          <p className={styles.footertitle}> Thank you for visiting Ksheer Sagar, we are delightful to serve you !</p>
+            {/* <p className={styles.footertitle}>
+              {" "}
+              Thank you for visiting Ksheer Sagar, we are delightful to serve
+              you !
+            </p> */}
           </div>
         </div>
       </div>
--- src/views/FeedbackForm/Invoice/ItemTable.js
+++ src/views/FeedbackForm/Invoice/ItemTable.js
@@ -1,19 +1,18 @@
 import React from "react";
 import styles from "./styles.module.css";
 
-const ItemTable = () => {
+const DigitalItemTable = () => {
   return (
     <div>
       <table className={styles.myTable}>
         <thead>
           <tr className={styles.bgColor4Cols}>
-            <th>item</th>
+            <th>Products</th>
+            <th>HSN/SAC</th>
             <th>Qty</th>
+
             <th>Price/Unit</th>
-          
-              <th>Price/Unit</th>
-              <th>Total</th>
-          
+            <th>Total</th>
           </tr>
         </thead>
 
@@ -27,10 +26,67 @@ const ItemTable = () => {
           </tr>
         </tbody>
       </table>
-      <hr/>
-    
+      <hr className={styles.hrLine} />
+      <div className={styles.totalInvoice}>
+        <div className={styles.titleTotal}>Total</div>
+        <div className={styles.titleTotal}>₹2230.00</div>
+      </div>
+      <div className={styles.totalInvoice}>
+        <div className={styles.titleTotal}>Round Off</div>
+        <div className={styles.titleTotal}>₹0.00</div>
+      </div>
+      <div className={styles.totalInvoice}>
+        <div className={styles.titleTotal}>Net Payable</div>
+        <div className={styles.titleTotal}>₹2230.00</div>
+      </div>
+      <div className={styles.totalInvoice}>
+        <div className={styles.titleTotalWord}>Amount in words</div>
+        <div className={styles.titleTotal}>
+          Two Thousand Two Hundred Thirty Rupees Only
+        </div>
+      </div>
+
+      {/* ////////// */}
+      <table className={styles.myTable}>
+        <thead>
+          <tr className={styles.bgColor4Cols}>
+            <th>GST%</th>
+            <th>Taxable</th>
+            <th>CGST</th>
+
+            <th>SGST</th>
+            <th>IGST</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>5%</td>
+            <td>₹1380.00</td>
+            <td>₹34.50</td>
+            <td>₹34.50</td>
+            <td>₹0.00</td>
+            <td>₹69.00</td>
+          </tr>
+        </tbody>
+      </table>
+      <hr className={styles.hrLine} />
+      <div className={styles.totalInvoice}>
+        <div className={styles.titleTotal}>Tender Tax</div>
+        <div className={styles.titleTotal}>₹2230.00</div>
+      </div>
+      <hr className={styles.hrLine} />
+      <div className={styles.totalInvoice}>
+        <div className={styles.titleTotal}>Tender Amount</div>
+        <div className={styles.titleTotal}>₹0.00</div>
+      </div>
+      <div className={styles.totalInvoice}>
+        <div className={styles.titleTotal}>Return Amount</div>
+        <div className={styles.titleTotal}>₹2230.00</div>
+      </div>
+      <hr className={styles.hrLine} />
     </div>
   );
 };
-
-export default ItemTable;
+export default DigitalItemTable;
