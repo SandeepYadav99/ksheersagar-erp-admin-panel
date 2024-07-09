e132d59ba62b4b465f638d0a6c5306db53260835
Sandeep Yadav <sandeepelectrovese@gmail.com>
2024-07-09 19:38:52 +0530

    Remove console.log

--- src/views/FeedbackForm/Invoice/Component/ProductTable.js
+++ src/views/FeedbackForm/Invoice/Component/ProductTable.js
@@ -1,45 +1,40 @@
-import React from 'react'
-import styles from "./Style.module.css"
-const ProductTable = ({posOder}) => {
+import React from "react";
+import styles from "./Style.module.css";
+const ProductTable = ({ posOder }) => {
   return (
-    <><table className={styles.myTable}>
-    <thead>
-      <tr className={styles.bgColor4Cols}>
-        <th>Products</th>
-        <th>HSN/SAC</th>
-        <th>Qty</th>
+    <>
+      <table className={styles.myTable}>
+        <thead>
+          <tr className={styles.bgColor4Cols}>
+            <th>Products</th>
+            <th>HSN/SAC</th>
+            <th>Qty</th>
 
-        <th>Price/Unit</th>
-        <th>GST%</th>
-        <th>Total</th>
-      </tr>
-    </thead>
-
-    <tbody>
-    {  console.log(posOder)}
-      {posOder?.products?.map((product, index) => {
-      
-        return (
-          <tr className={styles.hrlinetr}>
-            <td>{product?.name_en}</td>
-            <td>{product?.hsn || "N/A"}</td>
-            <td>
-              {product?.qty} {product?.unit}
-            </td>
-            <td>₹{product?.list_price}</td>
-            <td>
-              {" "}
-              {product?.gst
-                ? `${product?.gst}%`
-                : "0%"}
-            </td>
-            <td>₹{product?.price}</td>
+            <th>Price/Unit</th>
+            <th>GST%</th>
+            <th>Total</th>
           </tr>
-        );
-      })}
-    </tbody>
-  </table></>
-  )
-}
+        </thead>
 
-export default React.memo(ProductTable)
+        <tbody>
+          {posOder?.products?.map((product, index) => {
+            return (
+              <tr className={styles.hrlinetr}>
+                <td>{product?.name_en}</td>
+                <td>{product?.hsn || "N/A"}</td>
+                <td>
+                  {product?.qty} {product?.unit}
+                </td>
+                <td>₹{product?.list_price}</td>
+                <td> {product?.gst ? `${product?.gst}%` : "0%"}</td>
+                <td>₹{product?.price}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default React.memo(ProductTable);
